2b21c9e9653ff501d1c72ddd32e14475ceeb0e1d
oldkirillius <49608020+oldkirillius@users.noreply.github.com>
2022-12-27 14:29:36 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,21 +1,55 @@
+import sqlite3
 
-bookshelf = []
+
+
+
+
+def create_book_table():
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+
+    cursor.execute('CREATE TABLE IF NOT EXISTS books(name text primary key, author text, read integer)')
+    
+    connection.commit()
+    connection.close()
 
 
 def add_book(name, author):
-    bookshelf.append({'name':name, 'author':author, 'read':False})
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+
+    cursor.execute('INSERT INTO books VALUES (?, ?, 0)',(name, author))
+    
+    connection.commit()
+    connection.close()
+
 
 
 def get_all_books():
-    return bookshelf
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+    cursor.execute(f'Select * from books')
+    books = [{'name':row[0], 'author':row[1], 'read':row[2]} for row in cursor.fetchall()]
+    connection.close()
+    return books
+
+
 
 def mark_book_as_read(name):
-    for book in bookshelf:
-        if book['name'] == name:
-            book['read'] = True
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+
+    cursor.execute('UPDATE books SET read=1 WHERE name=?', (name,))
+    
+    connection.commit()
+    connection.close()
 
 
 def delete_book(name):
-    global bookshelf
-    bookshelf_temp = [book for book in bookshelf if book['name'] != name]
-    bookshelf = bookshelf_temp
+    connection = sqlite3.connect('data.db')
+    cursor = connection.cursor()
+
+    cursor.execute('DELETE FROM books WHERE name=?', (name,))
+   
+    connection.commit()
+    connection.close()
